ef37f43dc09f6fbabf464f5b77dfbd51bfafc52a
Gabriela-Maita <Gabriela.Maita@fundacion-jala.org>
2022-12-07 20:18:07 -0400

    Class2: Lives and score

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from dino_runner.components.game import Game
 
 if __name__ == "__main__":
      game = Game()
-     game.run()
-     print("hello there...")
+     death_count = 0
+     while game.running:
+          if not game.playing:
+               game.show_menu()
+               
+     
+    
